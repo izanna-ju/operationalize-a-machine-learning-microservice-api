73236147d4b647089cfe57b8d079eeefd2d7583a
Julius Izanna <izanna.rukky@gmail.com>
2023-04-11 17:38:46 +0000

    Update project files

--- run_kubernetes.sh
+++ run_kubernetes.sh
@@ -4,15 +4,18 @@
 
 # Step 1:
 # This is your Docker ID/path
-# dockerpath=<>
+dockerpath="izanna/udacity-devops-project"
 
 # Step 2
 # Run the Docker Hub container with kubernetes
-
+kubectl run udacity-devops-project\
+	--generator=run-pod/v1\
+	--image=$dockerpath\
+	--port=80 --labels app=udacity-devops-project
 
 # Step 3:
 # List kubernetes pods
-
+kubectl get pods
 # Step 4:
 # Forward the container port to a host
-
+kubectl port-forward udacity-devops-project 8000:80
--- upload_docker.sh
+++ upload_docker.sh
@@ -6,10 +6,14 @@
 # Step 1:
 # Create dockerpath
 # dockerpath=<your docker ID/path>
+dockerpath=izanna/udacity-devops-project
 
 # Step 2:  
 # Authenticate & tag
 echo "Docker ID and Image: $dockerpath"
+docker login
+docker --tag udacity-devops-project $dockerpath:latest
 
 # Step 3:
 # Push image to a docker repository
+docker push $dockerpath:latest
