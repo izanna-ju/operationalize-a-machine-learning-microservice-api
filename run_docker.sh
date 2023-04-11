505945836ebd349e4a4a22b158325f235c4a4082
Julius Izanna <izanna.rukky@gmail.com>
2023-04-11 17:18:09 +0100

    update project file

--- run_docker.sh
+++ run_docker.sh
@@ -4,9 +4,13 @@
 
 # Step 1:
 # Build image and add a descriptive tag
+docker build --tag=izanna/udacity-devops-project .
 
 # Step 2: 
 # List docker images
+docker image ls
 
 # Step 3: 
 # Run flask app
+#docker run -t udacity-devops-project bash
+docker run -p 8000:80 izanna/udacity-devops-project
